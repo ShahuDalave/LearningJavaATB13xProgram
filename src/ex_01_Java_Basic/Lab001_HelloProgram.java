a2e3293598aafc622297f63287dfc29584a093ae
ShahuD9119 <shahudalave20@gmail.com>
2025-06-07 14:42:30 +0530

    Hello Program Updated

--- src/ex_01_Java_Basic/Lab001_HelloProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloProgram.java
@@ -3,5 +3,7 @@ package ex_01_Java_Basic;
 public class Lab001_HelloProgram {
     public static void main(String[] args) {
         System.out.println("This is my first program");
+        System.out.println("This is my first program");
+        System.out.println("This is my first program");
     }
 }
