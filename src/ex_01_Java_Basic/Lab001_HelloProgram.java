0c0972a8febbe38ffa7b95c19a5f2f1caf4f8d47
ShahuD9119 <shahudalave20@gmail.com>
2025-06-07 14:46:25 +0530

    Hello Program Deleted

--- src/ex_01_Java_Basic/Lab001_HelloProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloProgram.java
@@ -3,7 +3,5 @@ package ex_01_Java_Basic;
 public class Lab001_HelloProgram {
     public static void main(String[] args) {
         System.out.println("This is my first program");
-        System.out.println("This is my first program");
-        System.out.println("This is my first program");
     }
 }
