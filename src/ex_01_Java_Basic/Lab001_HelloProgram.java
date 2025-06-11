00bce602742a531f7e174149fa8794918311cf83
ShahuD9119 <shahudalave20@gmail.com>
2025-06-12 00:17:25 +0530

    Table of nine

--- src/ex_01_Java_Basic/Lab001_HelloProgram.java
+++ src/ex_01_Java_Basic/Lab001_HelloProgram.java
@@ -2,6 +2,23 @@ package ex_01_Java_Basic;
 
 public class Lab001_HelloProgram {
     public static void main(String[] args) {
-        System.out.println("This is my first program");
+        int num=9;
+        //System.out.println("This is my first program")
+        System.out.printf("%d * %d = %d",9,1,9).println();
+        System.out.printf("%d * %d = %d",9,2,18).println();
+        System.out.printf("%d * %d = %d",9,3,27).println();
+        System.out.printf("%d * %d = %d",9,4,36).println();
+        System.out.printf("%d * %d = %d",9,5,45).println();
+        System.out.printf("%d * %d = %d",9,6,54).println();
+        System.out.printf("%d * %d = %d",9,7,63).println();
+        System.out.printf("%d * %d = %d",9,8,72).println();
+        System.out.printf("%d * %d = %d",9,9,81).println();
+        System.out.printf("%d * %d = %d",num,10,90).println();
+
+
     }
 }
+// %d-->> int,byte,long,short
+//%s-->> string
+//%f-->>float,double
+//%b-->>boolean
